db0dd23b1e13fee4819a681a00769736137b5fac
RA21709716 <RA21709716@AN0700118077305.dinfor.uniceub>
2017-11-23 20:53:03 -0200

    sadsad

---
+++ TrabalhoFinal/src/View/ViewExibeArtista.java
@@ -0,0 +1,11 @@
+package View;
+
+import javax.swing.JOptionPane;
+
+public class ViewExibeArtista {
+	public ViewExibeArtista(String artista){
+		
+		JOptionPane.showMessageDialog(null, artista);
+		
+	}
+}
